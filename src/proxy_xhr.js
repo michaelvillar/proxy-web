6e51f8be0b4c7a294afb45119e6a12ead88aa486
Michael Villar <me@michaelvillar.com>
2017-03-31 19:43:30 -0700

    Only prefix xhr if relative url

--- src/proxy_xhr.js
+++ src/proxy_xhr.js
@@ -2,6 +2,8 @@ var baseURL = '$0';
 var oldXHROpen = window.XMLHttpRequest.prototype.open;
 window.XMLHttpRequest.prototype.open = function(method, url) {
   var args = arguments;
-  args[1] = baseURL + encodeURIComponent(url);
+  if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+    args[1] = baseURL + encodeURIComponent(url);
+  }
   oldXHROpen.apply(this, args);
 };
